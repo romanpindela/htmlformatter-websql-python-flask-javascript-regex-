e66c3a53b41fc7fbea0ded82c6400e5175607b02
Roman Pindela <roman.pindela@gmail.com>
2021-03-07 11:07:43 +0100

    finished regular expressing that selects tags

--- main.js
+++ main.js
@@ -5,9 +5,21 @@ formatButton.addEventListener("click", formatHTML);
 const sourceArea = document.getElementById("source");
 const targetArea = document.getElementById("target");
 
+let stack = [];
+
+
 function formatHTML(){
     // 1st copy source innerHTML to target
     targetArea.textContent = sourceArea.value;
+    targetContent = targetArea.textContent;
+
+    // 2nd push htmls tags to stack
+    let regexpTag = /<(\/?(\w+))([^>]*)>([^<]*)/g;
+    let htmlTags = targetContent.matchAll(regexpTag);
+    for (let htmlTag of htmlTags){
+        console.log(htmlTag);
+    }
+
 
 
 }
