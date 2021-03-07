e37190ded166c5904a587eee1bd7411f9ed4a0e2
Roman Pindela <roman.pindela@gmail.com>
2021-03-07 10:01:09 +0100

    1st step - just copying text form source to target

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // put your code here
+let formatButton = document.getElementById("submit");
+formatButton.addEventListener("click", formatHTML);
+
+const sourceArea = document.getElementById("source");
+const targetArea = document.getElementById("target");
+
+function formatHTML(){
+    // 1st copy source innerHTML to target
+    targetArea.textContent = sourceArea.value;
+
+
+}
